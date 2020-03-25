6577787e4e2dca793795e95fe19ea463c4b6674b
Uzun Igor <te10@mail.ru>
2020-03-25 18:54:19 +0200

    make hw

---
+++ script.js
@@ -0,0 +1,28 @@
+function getDiscriminant (a, b, c) {
+  return b * b - 4 * a * c
+}
+
+function getRoot (a, b, c) {
+  var discriminant = getDiscriminant(a, b, c)
+
+  if (discriminant < 0) {
+    return {}
+  }
+  if (discriminant === 0) {
+    return {
+      x1: -(b / 2 * a),
+      x2: -b / 2 * a
+    }
+  }
+  if (discriminant > 0) {
+    return {
+      x1: (-b + Math.sqrt(discriminant)) / (2 * a),
+      x2: (-b - Math.sqrt(discriminant)) / (2 * a)
+    }
+  }
+}
+
+module.exports = {
+  getDiscriminant,
+  getRoot
+}
--- script.test.js
+++ script.test.js
@@ -1,15 +1,25 @@
-const obj = require('./scrpt')
+var obj = require('./script')
 
 describe('getDiscriminant', function () {
-  test('SHOULD return -7 when a = 4, b = 5, c = 2', function () {
-    expect(obj.getDiscriminant(4, 5, 2)).toEqual(-7)
+  test('a = 4, b = 5, c = 2 Reult = -7', function () {
+    expect(obj.getDiscriminant(4, 5, 2)).toBe(-7)
   })
-
-  test('SHOULD return 0 when a = 2, b = 4, c = 2', function () {
-    expect(obj.getDiscriminant(2, 4, 2)).toEqual(0)
+  test('a = 2, b = 5, c = 2 Reult = 9', function () {
+    expect(obj.getDiscriminant(2, 5, 2)).toBe(9)
   })
+  test('a = 3, b = 6, c = 3 Reult = 0', function () {
+    expect(obj.getDiscriminant(3, 6, 3)).toBe(0)
+  })
+})
 
-  test('SHOULD return 33 when a = 2, b = 7, c = 2', function () {
-    expect(obj.getDiscriminant(2, 7, 2)).toEqual(33)
+describe('getRoot', function () {
+  test('discriminant < 0 Result EMPTY: a = 4, b = 5, c = 2', function () {
+    expect(obj.getRoot(4, 5, 2)).toEqual({})
+  })
+  test('discriminant = 0 Result {x1: -1, x2: -1}:  a = 3, b = 6, c = 3', function () {
+    expect(obj.getRoot(3, 6, 3)).toEqual({ x1: -1, x2: -1 })
+  })
+  test('discriminant > 0 Result { x1: -0.5, x2: -2 }: a = 2, b = 5, c = 2', function () {
+    expect(obj.getRoot(2, 5, 2)).toEqual({ x1: -0.5, x2: -2 })
   })
 })
